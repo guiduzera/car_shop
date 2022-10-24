aec2ea25df96b80c0790fc2999b67619047f301c
Guilherme Eduardo <guigui.carvalho15@gmail.com>
2022-10-24 06:12:02 -0300

    resolvendo problema de xportacao

--- src/interfaces/IModel.ts
+++ src/interfaces/IModel.ts
@@ -1,10 +1,7 @@
-/* eslint-disable import/prefer-default-export */
-interface IModel<T> {
+export interface IModel<T> {
   create(obj: T): Promise<T>;
   read(): Promise<T[]>;
   readOne(str: string): Promise<T | null>;
   update(str: string, obj: T): Promise<T | null>;
   delete(str: string): Promise<T | null>;
 }
-
-export { IModel };
