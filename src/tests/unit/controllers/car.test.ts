b03a82fb3f6da052dc7209b17820f2f4ea88274c
Guilherme Eduardo <guigui.carvalho15@gmail.com>
2022-10-25 15:39:08 -0300

    realizando teste de camda controller com a funcao read

--- src/tests/unit/controllers/car.test.ts
+++ src/tests/unit/controllers/car.test.ts
@@ -4,7 +4,7 @@ import Car from '../../../models/Car';
 import CarService from '../../../services/CarService';
 import CarController from '../../../controllers/CarControllers';
 import { Request, Response } from 'express';
-import { createRequest } from '../mocks/carMocks';
+import { createRequest, readResult } from '../mocks/carMocks';
 const { expect } = chai;
 
 describe('Sua descrição', () => {
@@ -17,6 +17,7 @@ describe('Sua descrição', () => {
 
     before(() => {
         sinon.stub(carService, 'create').resolves(createRequest);
+        sinon.stub(carService, 'read').resolves(readResult);
     
         res.status = sinon.stub().returns(res);
         res.json = sinon.stub().returns(res);
@@ -32,6 +33,14 @@ describe('Sua descrição', () => {
             await carController.create(req, res);
             expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
             expect((res.json as sinon.SinonStub).calledWith(createRequest)).to.be.true
+        });
+    });
+
+    describe('quando o método read é chamado', () => {
+        it('corretamente lido!', async () => {
+            await carController.read(req, res);
+            expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+            expect((res.json as sinon.SinonStub).calledWith(readResult)).to.be.true
         });
     });
 });
